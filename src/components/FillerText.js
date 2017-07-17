5c95fa259dc6fd5c46b879527ab3429f8c41951c
Jason Sears <jsears4585@gmail.com>
2017-07-17 13:03:20 -0400

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class FillerText extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+    )
+  }
+}
+
+export default FillerText
